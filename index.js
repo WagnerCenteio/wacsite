10e3a5c2911cfca2e8762db7f854221318da155d
wac <centeio.wagner@gmail.com>
2020-03-29 19:53:04 -0100

    save animation status in localstorage

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 let i = 0;
-const cover = document.querySelector('.cover');
 const descrtptions = document.querySelectorAll('.animate');
+const isToAnimateDescription = localStorage.getItem('isDescriptionAnimated');
+const cover = document.querySelector('.cover');
 
-const runAnimation = setInterval(() => {
-	descrtptions[i].style.display = 'inline-block';
-	descrtptions[i].setAttribute('class', 'fade-in');
-	i++;
-	if (!descrtptions[i]) {
-		clearInterval(runAnimation);
-
-		setTimeout(() => {
-			cover.style.filter = 'none';
-		}, 1500);
-	}
-}, 1500);
+/**
+ ** FIXME:  Set the blur filter programaticaly instead of css hardcoded
+ */
+if (!isToAnimateDescription) {
+	const runAnimation = setInterval(() => {
+		descrtptions[i].style.display = 'inline-block';
+		descrtptions[i].setAttribute('class', 'fade-in');
+		i++;
+		if (!descrtptions[i]) {
+			clearInterval(runAnimation);
+			localStorage.setItem('isDescriptionAnimated', true);
+			setTimeout(() => {
+				cover.style.filter = 'none';
+			}, 1500);
+		}
+	}, 1500);
+} else {
+	descrtptions.forEach(item => (item.style.display = 'inline-block'));
+	cover.style.filter = 'none';
+}
